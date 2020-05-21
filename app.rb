414311167d3ddabe3909cb40e985493b381ab872
IrinaM-a <ruby19@bk.ru>
2020-05-21 13:54:43 +0500

    Init database, make @db

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'blog.rb'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
+
+configure do
+end
 
 
 get '/' do

54435c183f517c3cad4bd8c4f3e31bdf110f033f
IrinaM-a <ruby19@bk.ru>
2020-05-21 14:41:16 +0500

    create table posts

--- app.rb
+++ app.rb
@@ -13,6 +13,11 @@ before do
 end
 
 configure do
+  init_db
+  @db.execute 'CREATE TABLE IF NOT EXISTS posts
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              created_date DATE,
+              content TEXT)'
 end
 
 
